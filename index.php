2e998654a3f02917100fb7cbfce7a8a93417c7c1
unknown <you@example.com>
2020-04-23 23:43:01 +0100

    Added Styling

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
   include_once("lib/header.php");
 ?>
   <title>SNG Hospital</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
 
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <h5 class="my-0 mr-md-auto font-weight-normal">SNG Hospital</h5>
+    <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php">SNG Hospital</a></h5>
     <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="register.php">Register</a>
-      <a class="p-2 text-dark" href="login.php">Login</a>
+      <a class="p-2 text-dark" href="aboutus.php">About Us</a>
+      <a class="p-2 text-dark" href="contactus.php">Contact Us</a>
     </nav>
     </div>
 
