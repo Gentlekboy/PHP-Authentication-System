04c2928543e9279a077eff4acc0e72b4d55bbb23
unknown <you@example.com>
2020-05-05 23:24:49 +0100

    Verification of payment

--- process/processpay.php
+++ process/processpay.php
@@ -1,63 +1,73 @@
 <?php
-  session_start();
+    session_start();
 
-  require_once("../function/token.php");
-  require_once("../function/alert.php");
-  require_once("../function/redirect.php");
-  require_once("../function/user.php");
+    require_once("../function/token.php");
+    require_once("../function/alert.php");
+    require_once("../function/redirect.php");
+    require_once("../function/user.php");
 
-  $BillType = $_POST["BillType"];
-  $NGNCurrency = "NGN";
+    $BillType = $_POST['BillType'];
+    $NGNCurrency = 'NGN';
 
-  $_SESSION["BillType"] = $BillType;
-  $_SESSION["currency"] = $NGNCurrency;
+    $_SESSION['BillType'] = $BillType;
+    $_SESSION['currency'] = $NGNCurrency;
 
-  $curl = curl_init();
+    $curl = curl_init();
 
-  $customer_email = $_SESSION["email"];
-  $amount = $_SESSION["BillType"];  
-  $currency = $_SESSION["currency"];
-  $txref = "rave-2993baufaeOGfeuhjkhvg651568;"; // ensure you generate unique references per transaction.
-  $PBFPubKey = "FLWPUBK_TEST-9f69c64466b1aa52e4d240fd05cf61d6-X"; // get your public key from the dashboard.
-  $redirect_url = "http://localhost/SNGH/process/processtransact.php";
-  
+    $customer_email = $_SESSION['email'];
+    $amount = $_SESSION['BillType'];  
+    $currency = $_SESSION['currency'];
+
+    function generateTxtref(){
+      $txref = 'txref_rave-';
+      for ($i=0; $i < 20; $i++){ 
+        $txref .= mt_rand(0, 19);
+      }
+      return $txref;
+    }
+    $_SESSION['texref'] = generateTxtref();
+
+    $txref = $_SESSION['texref']; // ensure you generate unique references per transaction.
+    $PBFPubKey = "FLWPUBK_TEST-9f69c64466b1aa52e4d240fd05cf61d6-X"; // get your public key from the dashboard.
+    $redirect_url = "http://localhost/SNGH/testing.php";
+    
 
 
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/hosted/pay",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => json_encode([
-      'amount'=>$amount,
-      'customer_email'=>$customer_email,
-      'currency'=>$currency,
-      'txref'=>$txref,
-      'PBFPubKey'=>$PBFPubKey,
-      'redirect_url'=>$redirect_url,
-    ]),
-    CURLOPT_HTTPHEADER => [
-      "content-type: application/json",
-      "cache-control: no-cache"
-    ],
-  ));
-  
-  $response = curl_exec($curl);
-  $err = curl_error($curl);
-  
-  if($err){
-    // there was an error contacting the rave API
-    die('Curl returned error: ' . $err);
-  }
-  
-  $transaction = json_decode($response);
-  
-  if(!$transaction->data && !$transaction->data->link){
-    // there was an error from the API
-    print_r('API returned error: ' . $transaction->message);
-  }  
-  
-  // redirect to page so User can pay
-  // uncomment this line to allow the user redirect to the payment page
-  header('Location: ' . $transaction->data->link);
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/hosted/pay",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => json_encode([
+        'amount'=>$amount,
+        'customer_email'=>$customer_email,
+        'currency'=>$currency,
+        'txref'=>$txref,
+        'PBFPubKey'=>$PBFPubKey,
+        'redirect_url'=>$redirect_url,
+      ]),
+      CURLOPT_HTTPHEADER => [
+        "content-type: application/json",
+        "cache-control: no-cache"
+      ],
+    ));
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    if($err){
+      // there was an error contacting the rave API
+      die('Curl returned error: ' . $err);
+    }
+    
+    $transaction = json_decode($response);
+    
+    if(!$transaction->data && !$transaction->data->link){
+      // there was an error from the API
+      print_r('API returned error: ' . $transaction->message);
+    }  
+    
+    // redirect to page so User can pay
+    // uncomment this line to allow the user redirect to the payment page
+    header('Location: ' . $transaction->data->link);
   
 ?>
--- testing.php
+++ testing.php
@@ -1,47 +1,73 @@
 <?php
 session_start();
+require_once("function/token.php");
+require_once("function/alert.php");
+require_once("function/redirect.php");
+require_once("function/user.php");
+require_once("function/email.php");
 
-if ( isset( $_GET['txref'] ) ) {
-	$ref      = stripslashes( $_GET['txref'] );
-	$amount   = stripslashes( $_SESSION['BillType'] );// Correct Amount from Server
-	$currency = stripslashes( $_SESSION['currency'] ); // Correct Currency from Server
+    if (isset($_GET['txref'])) {
+        $ref = $_GET['txref'];
+        $amount = $_SESSION['BillType']; //Correct Amount from Server
+        $currency = $_SESSION['currency']; //Correct Currency from Server
+        $customer_email = $_SESSION['email'];
+				$txref = $_SESSION['texref'];
 
-	$query = array(
-		'SECKEY' => 'FLWSECK_TEST-c654a2e31a18c3d32779c1fce513fc06-X',
-		'txref'  => $ref,
-	);
+        $query = array(
+          "SECKEY" => "FLWSECK_TEST-c654a2e31a18c3d32779c1fce513fc06-X",
+          "txref" => $ref
+        );
 
-	$data_string = json_encode( $query );
+        $data_string = json_encode($query);
+                
+        $ch = curl_init('https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/verify');                                                                      
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                              
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
-	$ch = curl_init( 'https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/verify' );
-	curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, $data_string );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ) );
+        $response = curl_exec($ch);
 
-	$response = curl_exec( $ch );
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
 
-	$header_size = curl_getinfo( $ch, CURLINFO_HEADER_SIZE );
-	$header      = substr( $response, 0, $header_size );
-	$body        = substr( $response, $header_size );
+        curl_close($ch);
 
-	curl_close( $ch );
+        $resp = json_decode($response, true);
 
-	$resp = json_decode( $response, true );
+        $paymentStatus = $resp['data']['status'];
+        $chargeResponsecode = $resp['data']['chargecode'];
+        $chargeAmount = $resp['data']['amount'];
+        $chargeCurrency = $resp['data']['currency'];
 
-	$paymentStatus      = $resp['data']['status'];
-	$chargeResponsecode = $resp['data']['chargecode'];
-	$chargeAmount       = $resp['data']['amount'];
-	$chargeCurrency     = $resp['data']['currency'];
+        if (($chargeResponsecode == "00" || $chargeResponsecode == "0") && ($chargeAmount == $amount)  && ($chargeCurrency == $currency)) {
 
-	if ( ( $chargeResponsecode == '00' || $chargeResponsecode == '0' ) && ( $chargeAmount == $amount ) && ( $chargeCurrency == $currency ) ) {
-		header( 'Location: ../success.php' );
-		die();
+					$reciept = [
+						'currency'=>$currency,
+						'amount'=>$amount,
+						'customer_email'=>$customer_email,
+						'txref'=>$txref
+						];
+		
+					file_put_contents("db/payment/" . $customer_email . ".json", json_encode($reciept));
 
-	} else {
-		header( 'Location: ../failed.php' );
-	}
-} else {
-	die( 'No reference supplied' );
-}
+					set_alert("message", "We have recieved your payment");
+          header('Location: pay.php');
+            // transaction was successful...
+             // please check other things like whether you already gave value for this ref
+          // if the email matches the customer who owns the product etc
+          //Give Value and return to Success page
+        } else {
+					set_alert("message", "Payment failed");
+          header('Location: pay.php');
+          //Dont Give Value and return to Failure page
+        }
+    }
+        else {
+      die('No reference supplied');
+    }
+
+
+?>
