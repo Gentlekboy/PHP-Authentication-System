00c851cfbcf8d476d76e119084f7581b6a5755cd
unknown <you@example.com>
2020-05-05 23:26:13 +0100

    Tried sending mail

--- failed.php
+++ failed.php
@@ -1,1 +1,20 @@
-Transaction failed
+<?php
+session_start();
+require_once("function/token.php");
+require_once("function/alert.php");
+require_once("function/redirect.php");
+require_once("function/user.php");
+require_once("function/email.php");
+
+$email = $_SESSION['email'];
+
+$subject = "Payment Failed";
+$message = "We could not recieve your payment, please try again";
+$headers = "From: no-reply@sngh.org" . "\r\n" . "cc: kufre@sngh.org";
+
+$try = mail($email, $subject, $message, $headers);
+
+set_alert("message", "Payment failed");
+redirect_to("pay.php");
+
+?>
--- success.php
+++ success.php
@@ -1,1 +1,21 @@
-Transaction was successful.
+<?php
+session_start();
+require_once("function/token.php");
+require_once("function/alert.php");
+require_once("function/redirect.php");
+require_once("function/user.php");
+require_once("function/email.php");
+
+$email = $_SESSION['email'];
+//$amount = $_SESSION['BillType'];  
+
+$subject = "Payment Recieved";
+$message = "We have recieved your payment";
+$headers = "From: no-reply@sngh.org" . "\r\n" . "cc: kufre@sngh.org";
+
+$try = mail($email, $subject, $message, $headers);
+
+set_alert("message", "We have recieved your payment");
+redirect_to("pay.php");
+
+?>
